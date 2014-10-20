9afbc93645ab71dc518de0b616a31872fabe720c
ulrichugo@gmail.com <ulrichugo@gmail.com@231e256f-bcb5-533d-6f72-43546c97d2f7>
2014-10-20 01:39:43 +0000

    Alteração de ícones.

--- trunk/gen/br/com/aula/R.java
+++ trunk/gen/br/com/aula/R.java
@@ -22,8 +22,11 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int splashscreen=0x7f020001;
+        public static final int button_normal=0x7f020000;
+        public static final int button_on=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int splashscreen=0x7f020003;
+        public static final int star=0x7f020004;
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
