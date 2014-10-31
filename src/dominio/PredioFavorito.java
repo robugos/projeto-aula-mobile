6e814ed360ffb1f6c0b1ec392656a7a1f534782e
blendaguedes <blendaguedes@gmail.com>
2014-10-31 19:40:09 +0000

    Correção das classes de dominio.

---
+++ src/dominio/PredioFavorito.java
@@ -0,0 +1,28 @@
+package dominio;
+
+public class PredioFavorito {
+
+	private int id;
+	private Predio predio;
+	private Usuario usuario;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Predio getPredio() {
+		return predio;
+	}
+	public void setPredio(Predio predio) {
+		this.predio = predio;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+}
